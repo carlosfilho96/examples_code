e1a2ebaccb104e08646d301a8c472ed3a1e337b5
Carlos <carlosalmeidafilho96@gmail.com>
2024-06-25 12:56:31 -0300

    25062024_1256

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+  <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+  <title>Examples Code</title>
+</head>
+<body>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="jquery-1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>    
+
+  <?php
+    include 'pages/sistemaDeGrid.html';
+    include 'pages/tabelas.html';
+  ?>
+</body>
+</html>
